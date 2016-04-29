4f78ee9113139c342e7a22e40fd69d79d9472536
Andreas Heiberg <andreas@heiberg.io>
2016-04-29 22:39:57 +0100

    add views for product-groups and products

--- web/cafe/apps/stock/urls.py
+++ web/cafe/apps/stock/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^product-groups$', views.product_groups, name='product_groups'),
+    url(r'^product-groups/([0-9]+)$', views.product_group, name='product_group'),
+    url(r'^products$', views.products, name='products'),
+    url(r'^products/([0-9]+)$', views.product, name='product')
 ]
--- web/cafe/features/stock/steps/steps.py
+++ web/cafe/features/stock/steps/steps.py
@@ -3,3 +3,19 @@ from behaving.web.steps import *
 from behaving.sms.steps import *
 from behaving.mail.steps import *
 from behaving.personas.steps import *
+
+@step(u'I click "{name}"')
+def i_click(context, name):
+
+    element = context.browser.find_by_xpath(
+        ("//*[@id='%(name)s']|"
+         "//*[@name='%(name)s']|"
+         "//button[contains(string(), '%(name)s')]|"
+         "//input[@type='button' and contains(string(), '%(name)s')]|"
+         "//input[@type='button' and contains(@value, '%(name)s')]|"
+         "//input[@type='submit' and contains(string(), '%(name)s')]|"
+         "//input[@type='submit' and contains(@value, '%(name)s')]|"
+         "//a[contains(string(), '%(name)s')]") % {'name': name})
+    assert element, u'Element not found'
+    element.first.click()
+    
